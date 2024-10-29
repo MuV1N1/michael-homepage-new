6419fa350b79c3c08bd4c8a864796aa7d0f786a8
MuV1N1 <marvin.kleiner1910@gmail.com>
2024-10-29 18:48:02 +0100

    Color pallet according to card.jpg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vibrantPink: '#D32FB4',
+        brightPurple: '#9B4DF8',
+        skyBlue: '#4AB9FF',
+        limeGreen: '#A5E340',
+        deepOrange: '#FF7D2C',
+        softYellow: '#FFF76A',
+        backgroundBlack: '#000000',
+        whiteText: '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 };
