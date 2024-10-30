7ebde15bfa1836591b59dd02b9feb9275554e097
muv1n1 <marvin.kleiner1910@gmail.com>
2024-10-30 12:03:21 +0100

    Fixed navbar Background

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 
 export class NavBar extends Component {
   constructor(props) {
@@ -15,7 +15,7 @@ export class NavBar extends Component {
 
   render() {
     return (
-      <nav className="border-gray-200 dark:bg-gray-900 bg-gradient-to-b from-skyBlue/[.6] to-customBlack">
+      <nav className="border-gray-200 dark:bg-transaprent900 bg-gradient-to-b from-skyBlue/[.6] to-customBlack">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
             <i className="fa-solid fa-music h-50"></i>
@@ -23,7 +23,7 @@ export class NavBar extends Component {
           <button
             onClick={this.toggleNavbar} // Calls toggle function
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-transaprent100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-transaprent700 dark:focus:ring-gray-600"
             aria-controls="navbar-default"
             aria-expanded={this.state.isOpen}
           >
@@ -45,24 +45,24 @@ export class NavBar extends Component {
             </svg>
           </button>
           <div className={`${this.state.isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray-100[.6] dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transaprent100[.6] dark:bg-transaprent800 md:dark:bg-transaprent900 dark:border-gray-700">
               <li>
-                <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+                <a href="#" className="block py-2 px-3 text-white bg-transparent rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-300 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Über mich</a>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-transaprent md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-transaprent dark:hover:text-white md:dark:hover:bg-transparent">Über mich</a>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-300 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Equipment/Leistungen</a>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-transaprent md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-transaprent dark:hover:text-white md:dark:hover:bg-transparent">Equipment/Leistungen</a>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-300 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Galerie</a>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-transaprent md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-transaprent dark:hover:text-white md:dark:hover:bg-transparent">Galerie</a>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-300 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Kontakt</a>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-transaprent md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-transaprent dark:hover:text-white md:dark:hover:bg-transparent">Kontakt</a>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-300 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Impressum</a>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-transaprent md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-transaprent dark:hover:text-white md:dark:hover:bg-transparent">Impressum</a>
               </li>
             </ul>
           </div>
